b41e012269d916b3de6045c4a99ae4088ab44e5e
Uliinn <isakul@student.chalmers.se>
2026-01-01 13:08:36 +0100

    Day 3 part 2

---
+++ 3/2.py
@@ -0,0 +1,50 @@
+
+
+
+with open("input.txt","r") as f:
+  
+  corrupted_text = "".join([row.strip() for row in f.readlines()])
+  
+  corrupted_text = list(corrupted_text)
+  total = 0
+
+  i = 0
+  length = len(corrupted_text)  
+
+  enabled = True
+  while i < length:
+    if "".join(corrupted_text[i:i+4]) == "mul(" and enabled:
+      i += 4
+      start = i
+      
+      while corrupted_text[i].isdigit():
+        i += 1
+      firstNum = 0
+      if corrupted_text[i] == ',' and start != i:
+        firstNum = int("".join(corrupted_text[start:i]))
+      else:
+        continue
+      if firstNum >= 1000:
+        continue
+      i += 1
+
+      start = i
+      
+      while corrupted_text[i].isdigit():
+        i += 1
+      secondNum = 0
+      if corrupted_text[i] == ')' and start != i:
+        secondNum = int("".join(corrupted_text[start:i]))
+      else:
+        continue
+      if secondNum >= 1000:
+        continue
+
+      total += firstNum * secondNum
+    elif "".join(corrupted_text[i:i+4]) == "do()":
+      enabled = True 
+    elif "".join(corrupted_text[i:i+7]) == "don't()":
+      enabled = False
+    i += 1
+  
+  print(total)
