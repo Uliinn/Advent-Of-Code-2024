0c63d15a6b360401d8cf294a5f1628827650d9fb
Uliinn <isakul@student.chalmers.se>
2026-03-18 20:42:32 +0100

    Day 7 part 2

---
+++ 7/2.py
@@ -0,0 +1,37 @@
+# Now 3 leaves in recursion instead of 2. Still same recursion depth
+
+def recursive_check_possible(goal,current_sum,nums,nums_idx) -> bool:
+
+  if nums_idx == len(nums): return goal == current_sum
+
+  # add
+  if recursive_check_possible(goal,current_sum+nums[nums_idx],nums,nums_idx+1):
+    return True
+
+  # multiply
+  if recursive_check_possible(goal,current_sum*nums[nums_idx],nums,nums_idx+1):
+    return True
+  
+  # concatinate
+  concatination = int(str(current_sum) + str(nums[nums_idx])) # This is expensive, could surely be optimized
+  if recursive_check_possible(goal,concatination,nums,nums_idx+1):
+    return True
+  
+  return False
+
+
+with open("input.txt","r") as f:
+  inp = [row.strip() for row in f.readlines()]
+
+  equations = []
+  for i in range(len(inp)):
+    goal, nums = inp[i].split(":")
+    nums = [int(v) for v in nums.strip().split()]
+    equations.append((int(goal),nums))
+
+  total = 0
+  for goal, nums in equations:
+    if recursive_check_possible(goal,nums[0],nums,1): # nums_idx = 1 because we already added nums[0]
+      total += goal
+  
+  print(total)
