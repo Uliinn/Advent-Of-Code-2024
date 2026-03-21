bc42c27a26206f576ae7f11089d51f4c99fa2924
Uliinn <isakul@student.chalmers.se>
2026-03-21 12:12:32 +0100

    Day 10

---
+++ 10/1.py
@@ -0,0 +1,46 @@
+
+
+grid = []
+nine_coordinates = []
+with open("input.txt","r") as f:
+  for y,row in enumerate(f.readlines()):
+    grid.append([])
+    for x,v in enumerate(list(row.strip())):
+      val = int(v)
+      if val == 9:
+        nine_coordinates.append((y,x))
+      grid[y].append(val)
+
+width = len(grid[0])
+height = len(grid)
+#DFS from each 9 down to a zero.
+trails = 0
+for y_start,x_start in nine_coordinates:
+  stack = [(9,y_start,x_start)] # elevation, y, x
+  visited_zeroes = set()
+  while stack:
+    elevation, y, x = stack.pop()
+
+    if elevation == 0 and (y,x) not in visited_zeroes:
+      visited_zeroes.add((y,x))
+      trails += 1
+      continue
+
+    neighbours = [
+      (y-1, x), # up
+      (y+1, x), # down
+      (y, x-1), # left
+      (y,x+1)   # right
+    ]
+    
+    for n_y, n_x in neighbours:
+
+      if 0 <= n_y < height and 0 <= n_x < width:
+
+        new_elevation = grid[n_y][n_x]
+
+        if new_elevation + 1 == elevation:
+          stack.append((new_elevation,n_y,n_x))
+
+print(trails)
+
