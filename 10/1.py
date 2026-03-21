0b950449002a68efdb8817b9490dd04891aa2c25
Uliinn <isakul@student.chalmers.se>
2026-03-21 13:00:16 +0100

    Corrected order

--- 10/1.py
+++ 10/1.py
@@ -8,39 +8,39 @@ with open("input.txt","r") as f:
     for x,v in enumerate(list(row.strip())):
       val = int(v)
       if val == 9:
-        nine_coordinates.append((y,x))
+        nine_coordinates.append((x,y))
       grid[y].append(val)
 
 width = len(grid[0])
 height = len(grid)
 #DFS from each 9 down to a zero.
 trails = 0
-for y_start,x_start in nine_coordinates:
-  stack = [(9,y_start,x_start)] # elevation, y, x
+for x_start,y_start in nine_coordinates:
+  stack = [(9,x_start,y_start)] # elevation, x, y
   visited_zeroes = set()
   while stack:
-    elevation, y, x = stack.pop()
+    elevation, x, y = stack.pop()
 
-    if elevation == 0 and (y,x) not in visited_zeroes:
-      visited_zeroes.add((y,x))
+    if elevation == 0 and (x,y) not in visited_zeroes:
+      visited_zeroes.add((x,y))
       trails += 1
       continue
 
     neighbours = [
-      (y-1, x), # up
-      (y+1, x), # down
-      (y, x-1), # left
-      (y,x+1)   # right
+      (x, y-1), # up
+      (x, y+1), # down
+      (x-1, y), # left
+      (x+1,y)   # right
     ]
     
-    for n_y, n_x in neighbours:
+    for n_x, n_y in neighbours:
 
       if 0 <= n_y < height and 0 <= n_x < width:
 
         new_elevation = grid[n_y][n_x]
 
         if new_elevation + 1 == elevation:
-          stack.append((new_elevation,n_y,n_x))
+          stack.append((new_elevation,n_x,n_y))
 
 print(trails)
 
