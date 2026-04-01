7d94e1a2a5fdbd7b5679d25589f84ddddea296b5
Uliinn <isakul@student.chalmers.se>
2026-04-01 16:35:26 +0200

    Day 18 part 2 better version

--- 18/2.py
+++ 18/2.py
@@ -1,33 +1,40 @@
+
+# Was linear search, but can be binary search
+
 WIDTH, HEIGHT = 71, 71
 GOAL = WIDTH-1, HEIGHT-1
 
-CORRUPTED = 1
-SAFE = 0
-grid = [[SAFE for x in range(WIDTH)] for y in range(HEIGHT)]
-blocks = []
+block_to_time = dict()
+time_to_block = dict()
 with open("input.txt","r") as f:
   inp = f.readlines()
 
   for i in range(len(inp)):
     x,y = [int(v) for v in inp[i].strip().split(",")]
-    blocks.append((x,y))
+    block_to_time[(x,y)] = i
+    time_to_block[i] = (x,y)
 
 
-
+blocks = block_to_time.keys()
 
 # Dijkstra
 
-def dfs(grid,blocks):
+def dfs(k,blocks, blocks_to_time):
   """
   True if there is a path.
   False otherwise
   
   """
-  n_safe_spaces = WIDTH * HEIGHT - blocks
+  def block_present(x,y):
+    if (x,y) not in blocks:
+      return False 
+    
+    return blocks_to_time[(x,y)] <= k
+      
   stack = [(0,0)]
   visited = set()
 
-  while stack and len(visited) < n_safe_spaces:
+  while stack:
 
     (x,y) = stack.pop()
 
@@ -40,16 +47,20 @@ def dfs(grid,blocks):
 
     for xi,yi in [(x+1,y), (x,y-1),(x-1,y),(x,y+1)]:
       if 0 <= xi < WIDTH and 0 <= yi < HEIGHT:
-        if grid[yi][xi] == SAFE and (xi,yi) not in visited:
+        if  not block_present(xi,yi) and (xi,yi) not in visited:
           stack.append((xi,yi))
   return False
         
-n_blocks = 0
-for (x,y) in blocks:
 
-  grid[y][x] = CORRUPTED
-  n_blocks += 1
+lo, hi = 0, len(blocks)
+earliest_block = None
+while lo < hi:
+  k = lo + ((hi - lo) // 2)
 
-  if not dfs(grid, n_blocks):
-    print(f"{x},{y}")
-    break
+  if not dfs(k,blocks,block_to_time):
+    earliest_block = time_to_block[k]
+    hi = k
+  else:
+    lo = k + 1
+
+print(earliest_block)
