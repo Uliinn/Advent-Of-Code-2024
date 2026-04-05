1d8d09f505fe14290f68a93a476714851a0be5ed
Uliinn <isakul@student.chalmers.se>
2026-04-05 12:50:02 +0200

    Day 22 part 1 optimized

--- 22/1.py
+++ 22/1.py
@@ -1,29 +1,23 @@
-
-
+import numpy as np
 PRUNING_NUMBER = 16777216
 
 with open("input.txt","r") as f:
   start_values = [int(v.strip()) for v in f.readlines()]
+  start_values = np.asarray(start_values,dtype = np.int64)
 
 
 total = 0
-for start in start_values:
-  secret = start
-  for i in range(2000):
-    # Step 1
-    temp = secret << 6
-    secret = secret ^ temp
-    secret = secret % PRUNING_NUMBER
-    
-    # Step 2
-    temp = secret >> 5
-    secret = secret ^ temp
-    secret = secret % PRUNING_NUMBER
-    
-    #Step 3
-    temp = secret << 11
-    secret = secret ^ temp
-    secret = secret % PRUNING_NUMBER
+
+for i in range(2000):
+  # Step 1
+  start_values = ((start_values << 6) ^ start_values) % PRUNING_NUMBER
+  # Step 2
+  start_values = ((start_values >> 5) ^ start_values) % PRUNING_NUMBER
   
-  total += secret
-print(total)
+  #Step 3
+  start_values = ((start_values << 11) ^ start_values) % PRUNING_NUMBER
+
+    
+  
+  
+print(sum(start_values))
